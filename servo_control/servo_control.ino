12c767358244fa8b2de3f86ea0206de781f47cd6
James Donley <twocows360@gmail.com>
2012-10-09 17:43:21 -0400

    Swap out LED for servo (required no added code)

---
+++ servo_control/servo_control.ino
@@ -0,0 +1,45 @@
+/*
+  Servo control
+  Control a servo with a switch
+ */
+
+// Pin 13 is our servo
+int srv = 13;
+
+// Pin 2 is our switch output
+int pb = 2;
+
+void setup()
+{
+  // set pin 13 to be an output (writable)
+  pinMode(srv, OUTPUT);
+  // set pin 2 to be input data (readable)
+  pinMode(pb, INPUT);
+}
+
+// repeats ad nauseum
+void loop()
+{
+  // read button state
+  int buttonState = digitalRead(pb);
+  if (buttonState == HIGH)
+  {
+    act();
+  }
+  else
+  {
+    noAct();
+  }
+}
+
+// spin the servo
+void act()
+{
+  digitalWrite(srv, HIGH);
+}
+
+// stop servo
+void noAct()
+{
+  digitalWrite(srv, LOW);
+}
---
+++ servo_test/servo_test.ino
@@ -0,0 +1,29 @@
+/*
+  Basic Servo Test
+  Test how a servo works
+ */
+
+// Pin 13 is our servo's input
+int servo = 13;
+
+void setup()
+{
+  // set pin 13 to be writable as an output
+  pinMode(servo, OUTPUT);
+  // call our "action" function and see what happens when the servo is wired
+  act();
+  digitalWrite(servo, LOW);    // turn the LED off by making the voltage LOW
+}
+
+// unused for now
+void loop()
+{
+  
+}
+
+// output function test
+void act()
+{
+  digitalWrite(servo, HIGH);    // turn the LED on by making the voltage HIGH
+  delay(4000);
+}
